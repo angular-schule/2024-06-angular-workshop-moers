c764aead6e0162aecb1597536d694a7e6effb32d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-05 14:27:07 +0200

    alter / neuer Stil bei einfachen Werten

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  // alter Stil
   title = 'Book Rating';
+
+  // neuer Stil mit Signals
+  title2 = signal('Book Rating 2');
+
+  constructor() {
+    window.setTimeout(() => this.title = 'blubb', 1000);
+
+
+    window.setTimeout(() => this.title2.set('blubb'), 1000);
+  }
 }
