e22a95651e814bcabc36975a5ccdde0a40259562
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-07 10:34:22 +0200

    eventEmitter: alter Stil mit Dekorator

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input, model } from '@angular/core';
+import { Component, EventEmitter, Input, Output, computed, input, model } from '@angular/core';
 import { Book } from '../shared/book';
 import { NgClass } from '@angular/common';
 
@@ -12,14 +12,26 @@ import { NgClass } from '@angular/common';
 export class BookComponent {
 
   // alter Stil mit Dekorator
-  // @Input({ required: true }) book?: Book;
+  @Input({ required: true }) book?: Book;
 
   // tollesBuch() {
   //   return this.book?.title === 'Angular'
   // }
 
   // neuer Stil mit Signal --> DEVELOPER PREVIEW
-  book = input.required<Book>();
+  // book = input.required<Book>();
+  // tollesBuch = computed(() => this.book().title === 'Angular');
 
-  tollesBuch = computed(() => this.book().title === 'Angular');
+
+  // alter Stil mit Dekorator
+  @Output() rateUp = new EventEmitter<Book>();
+  @Output() rateDown = new EventEmitter<Book>();
+
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateRown() {
+    this.rateDown.emit(this.book);
+  }
 }
