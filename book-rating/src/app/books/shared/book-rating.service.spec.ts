7070d311ae06e954ff68cda94382d6cfcea02a38
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-06-07 13:37:51 +0200

    gruppenarbeit: testing

--- book-rating/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -28,6 +28,23 @@ fdescribe('BookRatingService', () => {
   it('should rate down a book by one', () => {
     const ratedBook = service.rateDown(book);
     expect(ratedBook.rating).toBe(2);
-  })
+  });
 
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should not mutate the book', () => {
+    const frozenBook = Object.freeze(book);
+    expect(() => service.rateUp(frozenBook)).not.toThrow();
+    expect(() => service.rateDown(frozenBook)).not.toThrow();
+  });
 });
